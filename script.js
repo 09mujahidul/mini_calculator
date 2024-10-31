0cc16cef701393fee6bce0164caf3a6679f9a1e3
Mujahidul Islam <164076552+09mujahidul@users.noreply.github.com>
2024-10-31 14:13:34 +0530

    update commit

--- script.js
+++ script.js
@@ -12,26 +12,26 @@ function sub(){
     let a=parseInt(document.getElementById("n1").value);
     let b=parseInt(document.getElementById("n2").value);
     let c=a-b;
-    document.getElementById("data1").innerHTML="Subtractn is = "+c;
+    document.getElementById("data1").innerHTML="Subtraction is = "+c;
 }
 function mul(){
 
     let a=parseInt(document.getElementById("n1").value);
     let b=parseInt(document.getElementById("n2").value);
-    document.getElementById("data1").innerHTML="Mulpcn is = "+a*b;
+    document.getElementById("data1").innerHTML="Mulptiplication is = "+a*b;
 }
 function sqadd(){
     let a=parseInt(document.getElementById("n1").value);
     let b=parseInt(document.getElementById("n2").value);
     let c=(a*a)+(b*b);
-    document.getElementById("data1").innerHTML="Sqr Add is = "+c;
+    document.getElementById("data1").innerHTML="Square Addition is = "+c;
 }
 
 function cb_add(){
     let a=parseInt(document.getElementById("n1").value);
     let b=parseInt(document.getElementById("n2").value);
     let cubadd=(a*a*a)+(b*b*b)
-    document.getElementById("data1").innerHTML="Cube Add is = "+cubadd;
+    document.getElementById("data1").innerHTML="Cube Addition is = "+cubadd;
 }
 function div(){
     let a=parseInt(document.getElementById("n1").value);
